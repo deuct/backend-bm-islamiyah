2a328d6795ed0e2606a37efd913e59483af858f4
farhannfdl <farhan972015@gmail.com>
2023-02-11 00:54:25 +0700

    development

--- helper/helperAll.js
+++ helper/helperAll.js
@@ -1,7 +1,7 @@
 export default (helperAll) => {
   const helperAllVal = {
     baseURL: "5000",
-    directory: "D:/project/bank mini/app/bac-end/filess",
+    directory: "D:/project/bank mini/app/bac-end/files",
   };
 
   return { helperAllVal };
