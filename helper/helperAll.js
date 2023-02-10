104a8327195fbfd3bd1fba09d2a972ab7c41f701
farhannfdl <farhan972015@gmail.com>
2023-02-11 00:30:42 +0700

    test git ignore

--- helper/helperAll.js
+++ helper/helperAll.js
@@ -1,6 +1,7 @@
 export default (helperAll) => {
   const helperAllVal = {
     baseURL: "5000",
+    directory: "D:/project/bank mini/app/back-end/files/s",
     directory: "/home/bank-mini/back-end/",
   };
 
