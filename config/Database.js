08cc4a5a540490bb08972b06b640bb177d7fb177
farhannfdl <farhan972015@gmail.com>
2023-02-11 01:21:17 +0700

    test

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("dbbm_smkislamiyah", "root", "", {
+const db = new Sequelize("dbbm_smkislamiyah", "root", "Root123!", {
   host: "127.0.0.1",
   dialect: "mysql",
 });
