c3ed9a085b30ae413ceca5b2c1d6d8c02b8b5c27
farhannfdl <farhan972015@gmail.com>
2023-02-23 08:25:24 +0700

    update 23 feb 2023 id transaksi & printdate

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("dbbm_smkislamiyah", "root", "Root123!", {
-  host: "localhost",
->>>>>>> deee495 (production update 12 feb 2023)
+  host: "127.0.0.1",
   dialect: "mysql",
 });
 
