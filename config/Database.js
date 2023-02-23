bf146137c19e79b901591b0a3bcbab2b82e47926
farhannfdl <farhan972015@gmail.com>
2023-02-23 08:23:31 +0700

    conflict database.js

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("dbbm_smkislamiyah", "root", "", {
-  host: "127.0.0.1",
+const db = new Sequelize("dbbm_smkislamiyah", "root", "Root123!", {
+  host: "localhost",
+>>>>>>> deee495 (production update 12 feb 2023)
   dialect: "mysql",
 });
 
